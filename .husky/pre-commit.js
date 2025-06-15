be361520d857244f9d68eee9c6e21efccfe95b32
Tony Ward <tonygward@googlemail.com>
2025-06-15 19:54:45 +0100

    feat(123): add new feature

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,17 +1,18 @@
 import { readFileSync } from "fs"
 
 const commitMessage = await readFileSync('.git/COMMIT_EDITMSG');
-console.log('Commit message:', commitMessage.toString().trim());
+// console.log('Commit message:', commitMessage.toString().trim());
 
-const regexPattern = '^(feat|fix|docs|style|refactor|perf|test|build|ci|temp)(\([a-zA-Z0-9_\-]+\))?:\s.+$';
-if (new RegExp(regexPattern).test(commitMessage)) {
+// const regexPattern = '^(feat|fix|docs|style|refactor|perf|test|build|ci|temp)(\([a-zA-Z0-9_\-]+\))?:\s.+$';
+const regexPattern = /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\([\w\-\.]+\))?(!)?: ([\w ])+([\s\S]*)/g;
+const regularExpression = new RegExp(regexPattern);
+if (regularExpression.test(commitMessage)) {
   console.log('Commit message is valid.');
   process.exit(0); // Simulate a successful validation
-} else {
-  console.log('Commit message is invalid.');
-  console.log('Please use the format: type(scope): description');
-  console.log('Where type is one of: feat, fix, docs, style, refactor, perf, test, build, ci, temp');
-  console.log('*** Example: feat(123): add new feature ***');
-  process.exit(1); // Simulate a failure
 }
 
+console.log('Commit message is invalid.');
+console.log('Please use the format: type(scope): description');
+console.log('Where type is one of: feat, fix, docs, style, refactor, perf, test, build, ci, temp');
+console.log('*** Example: feat(123): add new feature ***');
+process.exit(1); // Simulate a failure
